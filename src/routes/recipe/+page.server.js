dccbd8111f11992017d212418d15dafd3df7325d
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-11-08 02:11:19 +0100

    Created GraphQL request

---
+++ src/routes/recipe/+page.server.js
@@ -0,0 +1,33 @@
+import { gql } from "graphql-request";
+import { hygraph, hygraphHP } from "$lib/utils/hygraph";
+
+
+export async function load() {
+    let query = gql`
+    query getRecipe {
+        recipes {
+          linkSlug
+          recipeName
+          recipeDescription
+          preparationTime
+          recipeIngredients {
+            ingredientName
+            amount
+            unit
+          }
+          recipeInstructions
+          recipeDifficulty
+          tag
+          typeOfKitchen
+          author {
+            id
+            userName
+          }
+        }
+      }
+      `
+
+    const data = await hygraph.request(query)
+
+    return data
+}
