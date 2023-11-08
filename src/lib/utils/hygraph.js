8010487b60c466dfc38244eae2f2140bdbd5c34c
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-11-08 02:11:39 +0100

    Changed Endpoint URLS

--- src/lib/utils/hygraph.js
+++ src/lib/utils/hygraph.js
@@ -1,19 +1,25 @@
-import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private' 
+import { HYGRAPH_KEY, HYGRAPH_URL, HYGRAPH_URL_HIGH_PERFORMANCE } from '$env/static/private' 
 
 import { GraphQLClient } from 'graphql-request'
 
-export const hygraph = new GraphQLClient(HYGRAPH_URL, {
+ const headers = {
   headers: {
     Authorization: `Bearer ${HYGRAPH_KEY}`,
   },
-})
+}
+
+const headers2 = {
+  headers: {
+    Authorization: `Bearer ${HYGRAPH_KEY}`,
+  },
+}
 
 
-import { HYGRAPH_URL, HYGRAPH_URL_HIGH_PERFORMANCE } from '$env/static/private'
-import { GraphQLClient } from 'graphql-request'
+// import { HYGRAPH_URL, HYGRAPH_URL_HIGH_PERFORMANCE } from '$env/static/private'
+// import { GraphQLClient } from 'graphql-request'
 
 // Read and write
-export const hygraph = new GraphQLClient(HYGRAPH_URL)
+export const hygraph = new GraphQLClient(HYGRAPH_URL, headers)
 
 // Read only - High speed
-export const hygraphHP = new GraphQLClient(HYGRAPH_URL_HIGH_PERFORMANCE)
+export const hygraphHP = new GraphQLClient(HYGRAPH_URL_HIGH_PERFORMANCE, headers2)
