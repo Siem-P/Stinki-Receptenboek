75bde1c0ee183392fe31917fc7cd200431f307ef
Siem-P <64197688+Siem-P@users.noreply.github.com>
2023-12-22 14:13:44 +0100

    Short landingpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <!-- Homepage -->
 
 
+<h1>Welkom in het receptenboek van Sanne & Siem</h1>
+<p>Op deze website vind je alle recepten die wij lekker vinden. Je kunt ze hier vinden en zelfs toevoegen!</p>
 
-
-
+<a href="/recipe">Wat eten wij vandaag?</a>
 <style>
 
 </style>
